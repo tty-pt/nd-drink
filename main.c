548ab56608ba0bcf6c274a9491c7bdc07fc150e8
quirinpa <quirinpa@tty.pt>
2025-07-24 12:42:52 +0100

    Export fight

--- main.c
+++ main.c
@@ -1,16 +1,22 @@
-#include <nd/nd.h>
 #include "./include/uapi/drink.h"
+
 #include <stdio.h>
+
+#include <nd/nd.h>
+#include <nd/mortal.h>
 
 #define DRINK_VALUE (1 << 14)
 #define FOOD_VALUE(x) (1 << (16 - x->food))
 
-unsigned act_fill, act_drink, act_eat, type_consumable, vtf_pond;
+unsigned act_fill, act_drink, act_eat,
+	 type_consumable, vtf_pond;
+
+unsigned mortal_hd;
 
 void
 do_consume(int fd, int argc __attribute__((unused)), char *argv[]) {
 	unsigned player_ref = fd_player(fd), vial_ref;
-	ENT eplayer = ent_get(player_ref);
+	mortal_t mortal;
 	char *name = argv[1];
 	int aux;
 
@@ -27,23 +33,25 @@ do_consume(int fd, int argc __attribute__((unused)), char *argv[]) {
 		return;
 	}
 
-	CON *cvial = (CON *) &vial.sp.raw;
+	consumable_t *cvial = (consumable_t *) &vial.data;
 
 	if (!cvial->quantity) {
 		nd_writef(player_ref, "%s is empty.\n", vial.name);
 		return;
 	}
 
+	nd_get(mortal_hd, &mortal, &player_ref);
 	if (cvial->drink) {
-		aux = eplayer.huth[HUTH_THIRST] - DRINK_VALUE;
-		eplayer.huth[HUTH_THIRST] = aux < 0 ? 0 : aux;
+		aux = mortal.huth[HUTH_THIRST] - DRINK_VALUE;
+		mortal.huth[HUTH_THIRST] = aux < 0 ? 0 : aux;
 	}
 
 	if (cvial->food) {
-		aux = eplayer.huth[HUTH_HUNGER] - FOOD_VALUE(cvial);
-		eplayer.huth[HUTH_HUNGER] = aux < 0 ? 0 : aux;
+		aux = mortal.huth[HUTH_HUNGER] - FOOD_VALUE(cvial);
+		mortal.huth[HUTH_HUNGER] = aux < 0 ? 0 : aux;
 	}
 
+	nd_put(mortal_hd, &mortal, &player_ref);
 	cvial->quantity--;
 	OBJ player;
 	nd_get(HD_OBJ, &player, &player_ref);
@@ -55,8 +63,6 @@ do_consume(int fd, int argc __attribute__((unused)), char *argv[]) {
 		object_move(vial_ref, NOTHING);
 	else
 		nd_put(HD_OBJ, &vial_ref, &vial);
-
-	ent_set(player_ref, &eplayer);
 }
 
 void
@@ -73,7 +79,7 @@ do_fill(int fd, int argc __attribute__((unused)), char *argv[])
 
 	OBJ vial;
 	nd_get(HD_OBJ, &vial, &vial_ref);
-	CON *cvial = (CON *) &vial.sp.raw;
+	consumable_t *cvial = (consumable_t *) &vial.data;
 
 	if (vial.type != type_consumable || !cvial->capacity)
 		goto error;
@@ -84,7 +90,7 @@ do_fill(int fd, int argc __attribute__((unused)), char *argv[])
 	if (source.type != type_consumable)
 		goto error;
 
-	CON *csource = (CON *) &source.sp.raw;
+	consumable_t *csource = (consumable_t *) &source.data;
 	cvial->quantity = cvial->capacity;
 	cvial->drink = csource->drink;
 	cvial->food = csource->food;
@@ -101,13 +107,13 @@ error:
 struct icon on_icon(struct icon i, unsigned ref)
 {
 	OBJ obj;
-	CON *cwhat = (CON *) &obj.sp.raw;
+	consumable_t *cwhat = (consumable_t *) &obj.data;
 
 	nd_get(HD_OBJ, &obj, &ref);
 	if (obj.type != type_consumable)
 		return i;
 
-	cwhat = (CON *) &obj.sp.raw;
+	cwhat = (consumable_t *) &obj.data;
 
 	if (cwhat->drink) {
 		i.actions |= act_fill;
@@ -126,18 +132,18 @@ struct icon on_icon(struct icon i, unsigned ref)
 	return i;
 }
 
-int on_add(unsigned ref, uint64_t v)
+int on_add(unsigned ref, unsigned type, uint64_t v)
 {
 	OBJ obj;
 	SKEL skel;
-	CON *cnu = (CON *) &obj.sp.raw;
+	consumable_t *cnu = (consumable_t *) &obj.data;
 
-	nd_get(HD_OBJ, &obj, &ref);
-	if (obj.type != type_consumable)
+	if (type != type_consumable)
 		return 1;
 
+	nd_get(HD_OBJ, &obj, &ref);
 	nd_get(HD_SKEL, &skel, &obj.skid);
-	SCON *scon = (SCON *) &skel.sp.raw;
+	consumable_skel_t *scon = (consumable_skel_t *) &skel.data;
 	cnu->food = scon->food;
 	cnu->drink = scon->drink;
 
@@ -165,7 +171,7 @@ void mod_open(void *arg __attribute__((unused))) {
 	act_drink = action_register("drink", "🧪");
 	act_eat = action_register("eat", "🥄");
 
-	type_consumable = nd_put(HD_TYPE, NULL, "drink");
+	type_consumable = nd_put(HD_TYPE, NULL, "consumable");
 
 	nd_register("consume", do_consume, 0);
 	nd_register("fill", do_fill, 0);
@@ -175,6 +181,8 @@ void mod_open(void *arg __attribute__((unused))) {
 	/* sic_reg("on_icon"); */
 
 	SIC_AREG(on_consume);
+
+	nd_get(HD_HD, &mortal_hd, "mortal");
 }
 
 void mod_install(void *arg) {
